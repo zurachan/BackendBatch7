e71f03255c7e9720e01222667e8599db7ad52b16
Dương Hoàng Thái <manchestervn1996@gmail.com>
2024-08-30 09:40:39 +0700

    update development

---
+++ BackendBatch7.API/Controllers/PermissionController.cs
@@ -0,0 +1,16 @@
+﻿using BackendBatch7.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendBatch7.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionController : ControllerBase
+    {
+        [HttpGet]
+        public Response<bool> CheckPermission()
+        {
+            return new Response<bool>(true);
+        }
+    }
+}
